203bcdcf55ecf75ed3830d16f6e5aa23145775f6
nottomcline <tomcline.creator@gmail.com>
2026-05-05 13:13:40 +0200

    refactoring

--- 4. binary search/binarySearch.ts
+++ 4. binary search/binarySearch.ts
@@ -1,10 +1,9 @@
 function binarySearchIterative(arr: number[], x: number) {
   let low = 0;
   let high = arr.length - 1;
-  let mid;
 
   while (high >= low) {
-    mid = low + Math.floor((high - low) / 2);
+    const mid = low + Math.floor((high - low) / 2);
 
     if (arr[mid] === x) return mid;
 
